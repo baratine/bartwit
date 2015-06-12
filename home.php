b5a325142dc6e1cf25b5a986013d5029628c87b1
nam <nam@caucho.com>
2015-06-11 21:30:40 -0700

    use long php tags instead of short tags

--- home.php
+++ home.php
@@ -1,4 +1,4 @@
-<?
+<?php
 include("retwis.php");
 if (!isLoggedIn()) {
     header("Location: index.php");
@@ -21,7 +21,7 @@ include("header.php");
 <?=lookupTree("/tree/following:".$User['id'])->size()?> following<br>
 </div>
 </div>
-<?
+<?php
 $start = gt("start") === false ? 0 : intval(gt("start"));
 showUserPostsWithPagination(false,$User['id'],$start,10);
 include("footer.php")
